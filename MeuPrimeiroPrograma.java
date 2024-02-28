82d333f1f615a5456b7e55157fe82c62393be26f
makzs <erikmakiak@gmail.com>
2024-02-28 13:07:23 -0300

    commit 01

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,27 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+        System.out.println(2 + 2);
+
+
+        // variaveis -> Textos (String)
+        // variaveis -> Numeros Inteiros (Int)
+        // variaveis -> Numeros Fracionados (Double)
+        // variaveis -> Booleano (verdadeiro / falso)
+
+        var login = "Ola"; // string -> representação de textos
+        var number1 = 10; // int -> representação de numeros inteiros
+        var percent = 24.33; // double -> representação de numeros fracionados
+        var  isDriver = true; // boolean => representação booleana (estado verdadeiro/falso)
+
+        System.out.println(login);
+        System.out.println(number1);
+        System.out.println(percent);
+        System.out.println(isDriver);
+        
+        
+    }
+    
+
+}
