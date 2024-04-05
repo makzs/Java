41ee7c89431bdd5487adf4de3cf01750fb197410
makzs <erikmakiak@gmail.com>
2024-04-05 10:17:58 -0300

    aula10

--- Aulas/Aula09/Carro.java
+++ Aulas/Aula09/Carro.java
@@ -1,12 +1,74 @@
 public class Carro extends Veiculo{
 
+    // enumerador
+    public static enum Tipo {
+        uno,
+        bmw
+    }
+
     // conceito de encapsular a variavel é definir se ela é publica ou privada (proteger a informação ou nao)
+    // atributos
     private String marca;
+    private String modelo;
+    private int velocidadeMaxmia;
     private int velocidade = 0;
 
+    // construtor
+    public Carro (int velocidadeMaxmia){
+        this.velocidadeMaxmia = velocidadeMaxmia;
+    }
+
+    // metodo getter e setter
+
+    public int getVelocidadeMaxima(){
+        return velocidadeMaxmia;
+    }
+
+    // get marca (pegar)
+    public String getMarca(){
+        return marca;
+    }
+
+    // set marca (atribuir)
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
     public void acelerar(){
-        if (velocidade < 200){
+
+        if (this.velocidade < this.velocidadeMaxmia){
+            this.velocidade++;
+        }
+        /* 
+        if (modelo.equals(Tipo.uno) && velocidade < 200){
             velocidade++;
         }
+        else if (modelo.equals(Tipo.bmw) && velocidade < 240){
+            velocidade++;
+        }
+        */
     }
+
+    public void freiar(){
+        if (velocidade > 0){
+            velocidade--;
+        }
+    }
+
+
+    public int getVelocidade(){
+        return velocidade;
+    }
+
+    
+
 }
--- Aulas/Aula09/Programa.java
+++ Aulas/Aula09/Programa.java
@@ -3,12 +3,48 @@ public class Programa {
     // para acessar o valor do objeto se usa um metodo dele e nao a definição de valores, tudo para proteger a informação!
     public static void main(String[] args){
 
-        Carro uno = new Carro();
+        Carro uno = new Carro(200);
+
+        // definir a marca a partir de get e set
+        uno.setMarca("Fiat");
+        uno.setModelo("Uno Mille");
+
+        // utilizar os metodos da classe para alterar um atributo protegido com private
         uno.acelerar();
         uno.acelerar();
         uno.acelerar();
         uno.acelerar();
         uno.acelerar();
+
+        uno.freiar();
+        uno.freiar();
+        uno.freiar();
+
+        System.out.println("Marca: " + uno.getMarca());
+        System.out.println("Modelo: " + uno.getModelo());
+        System.out.println("Velocidade: " + uno.getVelocidade());
+        System.out.println("Velocidade: " + uno.getVelocidadeMaxima());
+
+        Carro bmw = new Carro(240);
+        bmw.setMarca("BMW");
+        bmw.setModelo("I7");
+
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.acelerar();
+        bmw.acelerar();
+
+        bmw.freiar();
+        bmw.freiar();
+        bmw.freiar();
+
+        System.out.println("Marca: " + bmw.getMarca());
+        System.out.println("Modelo: " + bmw.getModelo());
+        System.out.println("Velocidade: " + bmw.getVelocidade());
+        System.out.println("Velocidade: " + bmw.getVelocidadeMaxima());
     }
 
 }
